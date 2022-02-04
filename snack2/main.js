0ba41f545b4f1ff91de51466b7c120cb655e085d
Matteo Morganti <matteo99morganti@gmail.com>
2022-02-04 10:25:53 +0100

    push

--- snack2/main.js
+++ snack2/main.js
@@ -27,18 +27,27 @@ const team = [
     },
 
 ];
+ 
 
-
-
-    
+    const nomeFalli =[];
 
      for(let i=0; i<team.length; i++){
-        team[i].puntiFatti = getRandomNumber(1,90);
-        team[i].falliSubiti = getRandomNumber(250,500);
+        team[i].puntiFatti = getRandomNumber(0,3);
+        team[i].falliSubiti = getRandomNumber(0,50);
         console.log(team[i]);
-     }
+
+    }
+
+
+    for(let j=0; j<team.length; j++){
+        let {nome, falliSubiti} = team[j];
+        nomeFalli.push({nome, falliSubiti});
+        console.log(nomeFalli[j]);
+    }
 
 
     function getRandomNumber(min, max){
         return Math.floor(Math.random() * (max - min + 1) + min);
     }
+
+
